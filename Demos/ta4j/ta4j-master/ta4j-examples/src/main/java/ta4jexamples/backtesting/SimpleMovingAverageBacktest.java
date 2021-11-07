5fe1f4fbc61e42e1085a0c8e4a881e72c68afafc
Daniel Delijani <danieldelijani@gmail.com>
2021-11-07 13:15:26 -0500

    created new folder for demos, added backtesting folder

---
+++ Demos/ta4j/ta4j-master/ta4j-examples/src/main/java/ta4jexamples/backtesting/SimpleMovingAverageBacktest.java
@@ -0,0 +1,107 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 Marc de Verdelhan, 2017-2021 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package ta4jexamples.backtesting;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.ta4j.core.AnalysisCriterion;
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.BarSeriesManager;
+import org.ta4j.core.BaseBar;
+import org.ta4j.core.BaseBarSeries;
+import org.ta4j.core.BaseStrategy;
+import org.ta4j.core.Strategy;
+import org.ta4j.core.Trade;
+import org.ta4j.core.TradingRecord;
+import org.ta4j.core.analysis.criteria.pnl.GrossReturnCriterion;
+import org.ta4j.core.indicators.SMAIndicator;
+import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
+import org.ta4j.core.num.DecimalNum;
+import org.ta4j.core.num.Num;
+import org.ta4j.core.rules.OverIndicatorRule;
+import org.ta4j.core.rules.UnderIndicatorRule;
+
+public class SimpleMovingAverageBacktest {
+
+    public static void main(String[] args) throws InterruptedException {
+        BarSeries series = createBarSeries();
+
+        Strategy strategy3DaySma = create3DaySmaStrategy(series);
+
+        BarSeriesManager seriesManager = new BarSeriesManager(series);
+        TradingRecord tradingRecord3DaySma = seriesManager.run(strategy3DaySma, Trade.TradeType.BUY,
+                DecimalNum.valueOf(50));
+        System.out.println(tradingRecord3DaySma);
+
+        Strategy strategy2DaySma = create2DaySmaStrategy(series);
+        TradingRecord tradingRecord2DaySma = seriesManager.run(strategy2DaySma, Trade.TradeType.BUY,
+                DecimalNum.valueOf(50));
+        System.out.println(tradingRecord2DaySma);
+
+        AnalysisCriterion criterion = new GrossReturnCriterion();
+        Num calculate3DaySma = criterion.calculate(series, tradingRecord3DaySma);
+        Num calculate2DaySma = criterion.calculate(series, tradingRecord2DaySma);
+
+        System.out.println(calculate3DaySma);
+        System.out.println(calculate2DaySma);
+    }
+
+    private static BarSeries createBarSeries() {
+        BarSeries series = new BaseBarSeries();
+        series.addBar(createBar(CreateDay(1), 100.0, 100.0, 100.0, 100.0, 1060));
+        series.addBar(createBar(CreateDay(2), 110.0, 110.0, 110.0, 110.0, 1070));
+        series.addBar(createBar(CreateDay(3), 140.0, 140.0, 140.0, 140.0, 1080));
+        series.addBar(createBar(CreateDay(4), 119.0, 119.0, 119.0, 119.0, 1090));
+        series.addBar(createBar(CreateDay(5), 100.0, 100.0, 100.0, 100.0, 1100));
+        series.addBar(createBar(CreateDay(6), 110.0, 110.0, 110.0, 110.0, 1110));
+        series.addBar(createBar(CreateDay(7), 120.0, 120.0, 120.0, 120.0, 1120));
+        series.addBar(createBar(CreateDay(8), 130.0, 130.0, 130.0, 130.0, 1130));
+        return series;
+    }
+
+    private static BaseBar createBar(ZonedDateTime endTime, Number openPrice, Number highPrice, Number lowPrice,
+            Number closePrice, Number volume) {
+        return BaseBar.builder(DecimalNum::valueOf, Number.class).timePeriod(Duration.ofDays(1)).endTime(endTime)
+                .openPrice(openPrice).highPrice(highPrice).lowPrice(lowPrice).closePrice(closePrice).volume(volume)
+                .build();
+    }
+
+    private static ZonedDateTime CreateDay(int day) {
+        return ZonedDateTime.of(2018, 01, day, 12, 0, 0, 0, ZoneId.systemDefault());
+    }
+
+    private static Strategy create3DaySmaStrategy(BarSeries series) {
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
+        SMAIndicator sma = new SMAIndicator(closePrice, 3);
+        return new BaseStrategy(new UnderIndicatorRule(sma, closePrice), new OverIndicatorRule(sma, closePrice));
+    }
+
+    private static Strategy create2DaySmaStrategy(BarSeries series) {
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
+        SMAIndicator sma = new SMAIndicator(closePrice, 2);
+        return new BaseStrategy(new UnderIndicatorRule(sma, closePrice), new OverIndicatorRule(sma, closePrice));
+    }
+}
---
+++ Demos/ta4j/ta4j-master/ta4j-examples/src/main/java/ta4jexamples/loaders/CsvBarsLoader.java
@@ -0,0 +1,91 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2017 Marc de Verdelhan, 2017-2021 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package ta4jexamples.loaders;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.ta4j.core.BarSeries;
+import org.ta4j.core.BaseBarSeries;
+
+import com.opencsv.CSVReader;
+
+/**
+ * This class build a Ta4j bar series from a CSV file containing bars.
+ */
+public class CsvBarsLoader {
+
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    /**
+     * @return the bar series from Apple Inc. bars.
+     */
+    public static BarSeries loadAppleIncSeries() {
+        return loadCsvSeries("appleinc_bars_from_20130101_usd.csv");
+    }
+
+    public static BarSeries loadCsvSeries(String filename) {
+
+        InputStream stream = CsvBarsLoader.class.getClassLoader().getResourceAsStream(filename);
+
+        BarSeries series = new BaseBarSeries("apple_bars");
+
+        try (CSVReader csvReader = new CSVReader(new InputStreamReader(stream, Charset.forName("UTF-8")), ',', '"',
+                1)) {
+            String[] line;
+            while ((line = csvReader.readNext()) != null) {
+                ZonedDateTime date = LocalDate.parse(line[0], DATE_FORMAT).atStartOfDay(ZoneId.systemDefault());
+                double open = Double.parseDouble(line[1]);
+                double high = Double.parseDouble(line[2]);
+                double low = Double.parseDouble(line[3]);
+                double close = Double.parseDouble(line[4]);
+                double volume = Double.parseDouble(line[5]);
+
+                series.addBar(date, open, high, low, close, volume);
+            }
+        } catch (IOException ioe) {
+            Logger.getLogger(CsvBarsLoader.class.getName()).log(Level.SEVERE, "Unable to load bars from CSV", ioe);
+        } catch (NumberFormatException nfe) {
+            Logger.getLogger(CsvBarsLoader.class.getName()).log(Level.SEVERE, "Error while parsing value", nfe);
+        }
+        return series;
+    }
+
+    public static void main(String[] args) {
+        BarSeries series = CsvBarsLoader.loadAppleIncSeries();
+
+        System.out.println("Series: " + series.getName() + " (" + series.getSeriesPeriodDescription() + ")");
+        System.out.println("Number of bars: " + series.getBarCount());
+        System.out.println("First bar: \n" + "\tVolume: " + series.getBar(0).getVolume() + "\n" + "\tOpen price: "
+                + series.getBar(0).getOpenPrice() + "\n" + "\tClose price: " + series.getBar(0).getClosePrice());
+    }
+}
