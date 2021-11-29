fd90d16ce29621ae2eea2cb46a38464016df1307
samzorps <63939307+samzorps@users.noreply.github.com>
2021-11-28 22:57:22 -0500

    added functions for yahoo finance calls requestSpark and requestChart

--- Demos/yahoofinancedemo3/app/src/main/java/com/example/yahoofinancedemo3/MainActivity.java
+++ Demos/yahoofinancedemo3/app/src/main/java/com/example/yahoofinancedemo3/MainActivity.java
@@ -26,9 +26,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -74,7 +76,29 @@ public class MainActivity extends AppCompatActivity {
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        txtResponse.setText("Response: " + response.toString());
+                        double[][] finalData;
+                        JSONArray JSONopen;
+                        JSONArray JSONhigh;
+                        JSONArray JSONclose;
+                        JSONArray JSONlow;
+                        try {
+                            JSONArray result = response.getJSONObject("chart").getJSONArray("result");
+                            JSONObject indicators = result.getJSONObject(0).getJSONObject("indicators");
+                            JSONObject quote = indicators.getJSONArray("quote").getJSONObject(0);
+                            JSONopen = quote.getJSONArray("open");
+                            JSONhigh = quote.getJSONArray("high");
+                            JSONclose = quote.getJSONArray("close");
+                            JSONlow = quote.getJSONArray("low");
+                            finalData = new double[JSONopen.length()][4];
+                            for (int i=0; i < JSONopen.length(); i++){
+                                double[] row = {JSONopen.getDouble(i), JSONhigh.getDouble(i), JSONlow.getDouble(i), JSONclose.getDouble(i)};
+                                finalData[i] = row;
+                            }
+                            txtResponse.setText(Arrays.deepToString(finalData));
+                        } catch (Exception e) {
+                            e.getStackTrace();
+                        }
+
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -119,8 +143,101 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
     }
 
+    public void requestSpark(String[] tickers, String range, String interval){
+        Context act = getApplicationContext();
+        String strTickers = "";
+        for (String ticker : tickers){
+            strTickers += (ticker + ',');
+        }
+        strTickers = strTickers.substring(0, strTickers.length() - 1);
+
+        // SECRET KEY
+        String key = "3Z8LSHmB1l8lfS6qpRoba35QRos3zDZ69s2JS8IJ";
+        // Build URL
+        String url = "https://yfapi.net/v8/finance/spark?";
+        url = url + "interval=" + interval;    //add sampling interval
+        url = url + "&range=" + range;           //add range
+        url = url + "&symbols=" + strTickers;     //add ticker
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    /* Handles a JSON object response. Returns data in the form:
+                     * TODO: figure out format to return spark data
+                     */
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // parse response
+                        double[][] arrData;
+                        JSONObject stockData;
+                        JSONArray closePrices;
+
+                        try {
+                            int numDatapoints = response.getJSONObject(tickers[0]).getJSONArray("close").length();
+                            arrData = new double[response.length()][numDatapoints];
+                            for (int i=0; i<tickers.length; i++) {
+                                closePrices = response.getJSONObject(tickers[i]).getJSONArray("close");
+                                for(int c=0; c < closePrices.length(); c++) {
+                                    double price = closePrices.getDouble(c);
+                                    arrData[i][c] = price;
+                                }
+                            }
+                            txtResponse.setText(Arrays.deepToString(arrData));
+                        } catch (Exception e) {
+                            e.getStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("JSONObject VolleyError", "Error: " + error.getMessage());
+
+                        if (error instanceof TimeoutError) {
+                            Toast.makeText(act,
+                                    "Bad Network, Try again",
+                                    Toast.LENGTH_LONG).show();
+                        } else if (error instanceof NoConnectionError) {
+                            Toast.makeText(act,
+                                    "Bad Network, Try again",
+                                    Toast.LENGTH_LONG).show();
+                        } else if (error instanceof AuthFailureError) {
+                            Toast.makeText(act,
+                                    "Auth failed",
+                                    Toast.LENGTH_LONG).show();
+                        } else if (error instanceof ServerError) {
+                            Toast.makeText(act,
+                                    "Server Not Responding",
+                                    Toast.LENGTH_LONG).show();
+                        } else if (error instanceof NetworkError) {
+                            Toast.makeText(act,
+                                    "Network Error",
+                                    Toast.LENGTH_LONG).show();
+                        } else if (error instanceof ParseError) {
+                            Toast.makeText(act,
+                                    "try again"+error.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("accept", "application/json");
+                params.put("X-API-KEY", key);
+                return params;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
     public void reqOnClick(View view){
         String ticker = etxtTicker.getText().toString();
         requestChart(ticker);
     }
+
+    public void reqSpkOnClick(View view){
+        String[] tickers = {"AMZN", "msft", "CGC"};
+        requestSpark(tickers, "5d", "1d");
+    }
+
 }
